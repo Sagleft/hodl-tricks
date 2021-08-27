e0599150e4912daa23feb8cb8da754fe2c90780a
Sagleft <sagleft@mfc.click>
2021-08-28 03:34:57 +0400

    update errors handling

--- generate.go
+++ generate.go
@@ -1,12 +1,17 @@
 package main
 
 import (
+	"errors"
+	"io/ioutil"
 	"log"
+	"math/rand"
+	"os"
 	"strings"
 )
 
 const (
-	codeTtPath = "main_template.go.raw"
+	codeTtPath          = "main_template.go.raw"
+	destinationCodePath = "build/main.go"
 )
 
 type solution struct {
@@ -45,6 +50,49 @@ func (sol *solution) updateCode() error {
 }
 
 func (sol *solution) saveCode() error {
-	// TODO
+	return SaveStringToFile(destinationCodePath, sol.CodeTt)
+}
+
+// ReadFileToString read file to string
+func ReadFileToString(filepath string) (string, error) {
+	file, err := ioutil.ReadFile(filepath)
+	if err != nil {
+		return "", errors.New("failed to read file: " + err.Error())
+	}
+	return string(file), nil
+}
+
+type errorFunc func() error
+
+func checkErrors(errChecks ...errorFunc) error {
+	for _, errFunc := range errChecks {
+		err := errFunc()
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func getRandomString(length int) string {
+	var symbolRunes = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
+	b := make([]rune, length)
+	for i := range b {
+		b[i] = symbolRunes[rand.Intn(len(symbolRunes))]
+	}
+	return string(b)
+}
+
+// SaveStringToFile save arbitrary string to file
+func SaveStringToFile(filepath string, content string) error {
+	file, err := os.Create(filepath)
+	if err != nil {
+		return errors.New("failed to create file: " + err.Error())
+	}
+	defer file.Close()
+	_, err = file.WriteString(content)
+	if err != nil {
+		return errors.New("failed to write string to file: " + err.Error())
+	}
 	return nil
 }

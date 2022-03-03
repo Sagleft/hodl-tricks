f9c7d3fcf78b8fcf86dceefaace9399a111c1d45
Sagleft <sagleft@mfc.click>
2022-03-03 03:14:14 +0200

    update build script

--- build_win.sh
+++ build_win.sh
@@ -1,8 +1,10 @@
 echo "create new key.."
 go run generate.go
 cd generated_code
+set GOOS=windows
+set GOARCH=amd64
 go build
-mv gen ../build/tool
+mv gen.exe ../build/tool.exe
 cd ..
 #echo "hide last key, generate new.."
 #go run generate.go
